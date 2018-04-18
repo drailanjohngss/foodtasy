6243755bd4a99a04b4b35328a961b3c6512fefee
Eruel Parada <eruelparada@gmail.com>
2018-04-18 16:57:28 +0800

    Add separate file for routes

--- server.js
+++ server.js
@@ -2,9 +2,24 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 5000;
+const routes = require('./routes/index');
 
-app.get('/api/hello', (req, res) => {
-	res.send({ express: 'Hello From Express' });
+app.use('/', routes);
+
+app.use(function(req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
